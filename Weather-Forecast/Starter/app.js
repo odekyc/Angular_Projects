e234bfbd05ece55d255a98339e192db981e3ced7
Elva Chao <odekyc@Elvas-Air.attlocal.net>
2016-06-15 12:22:22 -0700

    hi

--- Weather-Forecast/Starter/app.js
+++ Weather-Forecast/Starter/app.js
@@ -12,6 +12,10 @@ weatherApp.config(function($routeProvider){
      .when( '/forecast', {
         templateUrl : 'pages/forecast.htm' ,
         controller : 'forecastController'
+     })
+     .when( '/forecast/:days', {
+        templateUrl : 'pages/forecast.htm' ,
+        controller : 'forecastController'
      });
 });
 
@@ -36,6 +40,23 @@ weatherApp.controller('homeController', [ '$scope' , 'cityService' , function($s
 }]);
 
 
-weatherApp.controller('forecastController', [ '$scope' , 'cityService',  function($scope, cityService){
+weatherApp.controller('forecastController', [ '$scope' , '$resource' , '$routeParams', 'cityService',  function($scope , $resource , $routeParams , cityService){
     $scope.city=cityService.city;
+
+    $scope.days=$routeParams.days || 2;
+
+    $scope.weatherAPI=$resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=ef0046d53dea5f5635696973098f4338", {
+    	callback: "JSON_CALLBACK"}, { get : {method: "JSONP"}});
+     
+     $scope.weatherResult=$scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+     
+
+    $scope.convertToFah=function(degK){
+    	return Math.round((1.8 * (degK -273)) + 32);
+    };
+
+    $scope.convertToDate=function( datems){
+        return new Date( datems*1000);
+    };
 }]);
