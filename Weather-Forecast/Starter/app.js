6a567b7eeaa454a53908c46f116ea21912eaee8e
Elva Chao <odekyc@Elvas-Air.championbroadband.com>
2016-06-15 01:14:05 -0700

    first angular project ^^

---
+++ Weather-Forecast/Starter/app.js
@@ -0,0 +1,41 @@
+// module
+
+var weatherApp = angular.module('weatherApp', [ 'ngRoute' , 'ngResource'] );
+
+//ROUTES
+weatherApp.config(function($routeProvider){
+     $routeProvider
+     .when( '/' , {
+     	templateUrl : 'pages/home.htm' ,
+     	controller : 'homeController'
+     })
+     .when( '/forecast', {
+        templateUrl : 'pages/forecast.htm' ,
+        controller : 'forecastController'
+     });
+});
+
+//SERVICES
+
+weatherApp.service('cityService', function(){
+     this.city="Arcadia, CA";
+
+});
+
+//CONTROLLERS
+
+weatherApp.controller('homeController', [ '$scope' , 'cityService' , function($scope, cityService){
+   $scope.city=cityService.city;
+
+   $scope.$watch('city', function(){
+      cityService.city=$scope.city;
+
+   });
+
+
+}]);
+
+
+weatherApp.controller('forecastController', [ '$scope' , 'cityService',  function($scope, cityService){
+    $scope.city=cityService.city;
+}]);
