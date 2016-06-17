6e0620e45936ac02ec6decfc01c55f4cb82caa72
Elva Chao <odekyc@Elvas-Air.lan>
2016-06-16 23:08:40 -0700

    hi

---
+++ Template_Store/app/templates/templates.js
@@ -0,0 +1,31 @@
+angular.module('templateStore.templates', [ 'ngRoute'])
+.config(['$routeProvider', function($routeProvider){
+     $routeProvider.
+     when('/templates', {
+     	templateUrl: 'templates/templates.html',
+     	controller: 'TemplatesCtrl'
+     }).
+      when('/templates/:templateId', {
+     	templateUrl: 'templates/details.html',
+     	controller: 'DetailsCtrl'
+     })
+}])
+.controller('TemplatesCtrl', ['$scope', '$http',  function($scope, $http){
+	console.log('templatescontroller init');
+	$http.get('/JSON/templates.json').success(function(response){
+		console.log("success response");
+		$scope.templates=response;
+	});
+}])
+.controller('DetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+	console.log('details');
+	var templateId= $routeParams.templateId;
+	$http.get('/JSON/templates.json').success(function(response){
+		console.log("success response");
+		$scope.mytemplate=$filter('filter')(response, function(d){
+             return d.id == templateId;
+		})[0];
+
+	});
+	
+}])
