4f7d859be0a762a635488bc321e2078aff84e5c2
Elva Chao <odekyc@Elvas-Air.lan>
2016-06-20 13:59:07 -0700

    done

--- My_Contacts/app/contacts/contacts.js
+++ My_Contacts/app/contacts/contacts.js
@@ -79,6 +79,55 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
         });
     }
 
+    $scope.editFormSubmit=function(){
+        console.log("Updating Contact.....");
+        //get ID
+
+        var id=$scope.id;
+
+        //get record
+        var record=$scope.contacts.$getRecord(id);
+
+        record.name= $scope.name;
+        record.email=$scope.email;
+        record.company=$scope.company;
+        record.phones[0].work=$scope.work_phone;
+        record.phones[0].home=$scope.home_phone;
+        record.phones[0].mobile=$scope.mobile_phone;
+        record.address[0].street_address=$scope.street_add;
+        record.address[0].city=$scope.city;
+        record.address[0].state=$scope.state;
+        record.address[0].zipcode=$scope.zipcode;
+
+        //save contact
+
+        $scope.contacts.$save(record).then(function(ref){
+             console.log(ref.key);
+        });
+
+        clearFields();
+
+        //hide form
+        $scope.editFormShow=false;
+        
+        $scope.msg="Contact Updated";
+    }
+
+    $scope.showEditForm=function(contact){
+          $scope.editFormShow=true;
+
+          $scope.id =contact.$id;
+          $scope.name=contact.name;
+          $scope.email=contact.email;
+          $scope.company=contact.company;
+          $scope.work_phone=contact.phones[0].work;
+          $scope.home_phone=contact.phones[0].home;
+          $scope.mobile_phone=contact.phones[0].mobile;
+          $scope.street_add=contact.address[0].street_address;
+          $scope.city=contact.address[0].city;
+          $scope.state=contact.address[0].state;
+          $scope.zipcode=contact.address[0].zipcode;
+    }
 
     
     $scope.showContact=function(curcontact){
@@ -96,7 +145,15 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
 
         $scope.contactShow=true;
     }
-    
+
+    $scope.removeContact=function(curcontact){
+
+        console.log("removing contact");
+        $scope.contacts.$remove(curcontact);
+
+        $scope.msg="Contact Removed";
+    }
+
     var clearFields=function(){
         console.log("clear fields....");
          $scope.name='';
